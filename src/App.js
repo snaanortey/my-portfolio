a76af11a66f33c3ead51a7a0a7bc7a82addb1af1
Naa Norkor <nortey.sarah@gmail.com>
2022-11-22 10:34:51 -0500

    updates to mock UI

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
+import profilepic from "../src/assets/images/profile-pic.jpg";
+import github from "../src/assets/icons/github.svg";
+import instagram from "../src/assets/icons/instagram.svg";
+import linkedin from "../src/assets/icons/linkedin.svg";
+
 // import {Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi, my name is Naa Norkor</h1>
-      <h3>I'm a full stack web developer</h3>
-      <h4>
-        For now, this website is under construction. I'm lookinat at completing
-        it by the second week of December 2022.
-      </h4>
-      <h3>Here are my contact details:</h3>
-      <a href="https://github.com/snaanortey" rel="noreferrer" target="_blank">Github</a>
-      <br />
-      <a href="https://www.linkedin.com/in/naanorkornortey/" rel="noreferrer" target="_blank">LinkedIn</a>
+      <div className="profile-pic-container">
+        <img className="profile-pic" src={profilepic} alt="profile pic" />
+      </div>
+      <div className="content">
+        <h1 className="name">Naa Norkor</h1>
+        <h3 className="title">Full stack web developer</h3>
+        <h4>
+          For now, this website is under construction. I'm lookinat at
+          completing it by the second week of December 2022.
+        </h4>
+        <h3>Let's connect!</h3>
+        <div className="icons">
+          <a className="icon-link"
+            href="https://github.com/snaanortey"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img className="icon-image" src={github} alt="github icon" />
+          </a>
+          <br />
+          <a className="icon-link"
+            href="https://www.linkedin.com/in/naanorkornortey/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img className="icon-image" src={linkedin} alt="linkedin icon" />
+          </a>
+          <a className="icon-link"
+            href="https://www.instagram.com/naanorks/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <img className="icon-image" src={instagram} alt="instagram icon" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
